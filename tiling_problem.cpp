2a31d4f77782351cc43940b3c5fbceb99bca9144
anitesh24 <68590286+anitesh24@users.noreply.github.com>
2020-07-25 20:24:25 +0530

    Create tiling_problem.cpp

---
+++ tiling_problem.cpp
@@ -0,0 +1,23 @@
+// tile a floor of size n x m using 1 x m tiles 
+#include <bits/stdc++.h> 
+using namespace std; 
+int countWays(int n, int m) 
+{ 
+    int count[n + 1]; 
+    count[0] = 0; 
+    for (int i = 1; i <= n; i++) { 
+        if (i > m) 
+            count[i] = count[i - 1] + count[i - m]; 
+        else if (i < m || i == 1) 
+            count[i] = 1; 
+        else
+            count[i] = 2; 
+    } 
+    return count[n]; 
+} 
+int main() 
+{ 
+    int n = 7, m = 4; 
+    cout << countWays(n, m); 
+    return 0; 
+} 
